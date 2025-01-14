da2b3b90222805b55306ce17b17fe28bf0150237
JOY <117473600+imjoyhalder@users.noreply.github.com>
2025-01-14 22:28:19 +0600

    Add files via upload

---
+++ January/Day-13/Coins_And_Triangle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t; 
+    while(t--){
+        int n; cin>>n; 
+	    int h = (-1 + sqrt(1 + 8.0 * n)) / 2;
+        cout<<h<<endl; 
+    }
+	
+}
