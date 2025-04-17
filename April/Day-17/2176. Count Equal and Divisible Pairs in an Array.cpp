9a503c3fd2bb75996ea29849e429519e9b899307
JOY <117473600+imjoyhalder@users.noreply.github.com>
2025-04-17 19:46:18 +0600

    Add files via upload
    Day-17 (2176. Count Equal and Divisible Pairs in an Array)

---
+++ April/Day-17/2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int countPairs(vector<int>& nums, int k) {
+            unordered_map<int, unordered_map<int, int>> moduloMap;
+            int res = 0;
+    
+            for (int i = 0; i < nums.size(); i++) {
+                int mod_i = i % k;
+    
+                if (moduloMap.count(nums[i])) {
+                    for (auto& [mod_j, count] : moduloMap[nums[i]]) {
+                        if ((mod_i * mod_j) % k == 0) {
+                            res += count;
+                        }
+                    }
+                }
+                moduloMap[nums[i]][mod_i]++;
+            }
+            return res;
+        }
+    };
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    
+      
+    return 0;
+} 
