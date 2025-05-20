27e348c8f815b816daf554f17145239dae5a59bc
JOY <joyhalder00113355@gmail.com>
2025-05-20 22:15:12 +0600

    Add files via upload
    Day-20 (3355. Zero Array Transformation I)

---
+++ Mey/Day-20/3355. Zero Array Transformation I.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int p[100001] = {0};
+class Solution {
+public:
+    static bool isZeroArray(const vector<int>& a, const vector<vector<int>>& q) {
+        const int n = a.size();
+        const int m = q.size();
+        memset(p, 0, sizeof(int) * (n + 1));
+        for (int i = 0; i < m; ++i) {
+            ++p[q[i][0]];
+            --p[q[i][1] + 1];
+        }
+        for (int i = 0, d = 0; i < n; ++i) {
+            d += p[i];
+            if (a[i] - d > 0) return false;
+        }
+        return true;
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    
+      
+    return 0;
+} 
